ef50ca375874e4e2473b982a2ff1be1b93126874
iqbalalhuda <38303303+iqbalalhuda@users.noreply.github.com>
2018-08-02 04:04:12 +0700

    Add files via upload

---
+++ view/index.php
@@ -0,0 +1,133 @@
+<?php
+//including the database connection file
+include_once("config.php");
+
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM pesanan"); // using mysqli_query instead
+?>
+
+<html lang="en">
+<head>
+	<title>Table V01</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<head>	
+	<title>Homepage</title>
+</head>
+
+<body>
+<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
+				<table>
+						<thead>
+							<tr class="table100-head">
+								<th class="column1">No</th>
+								<th class="column2">Doctor Name</th>
+								<th class="column3">Patient Name</th>
+								<th class="column4">Specialization</th>
+								<th class="column5">Consultancy Fee</th>
+								<th class="column6">Appointment Date / Time</th>
+								<th class="column7">Appointment Creation Date</th>
+								<th class="column7">Current Status</th>
+								<th class="column7">Action</th>
+							</tr>
+						</thead>
+	
+	
+	
+	<?php
+$sql=mysqli_query($mysqli,"select doctors.doctorName as docname,users.fullName as pname,appointment.*  from appointment join doctors on doctors.id=appointment.doctorId join users on users.id=appointment.userId ");
+$cnt=1;
+while($row=mysqli_fetch_array($sql))
+{
+?>
+
+											<tr>
+												<td class="center"><?php echo $cnt;?>.</td>
+												<td class="hidden-xs"><?php echo $row['docname'];?></td>
+												<td class="hidden-xs"><?php echo $row['pname'];?></td>
+												<td><?php echo $row['doctorSpecialization'];?></td>
+												<td><?php echo $row['consultancyFees'];?></td>
+												<td><?php echo $row['appointmentDate'];?> / <?php echo
+												 $row['appointmentTime'];?>
+												</td>
+												<td><?php echo $row['postingDate'];?></td>
+												<td>
+<?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+{
+	echo "Active";
+}
+if(($row['userStatus']==0) && ($row['doctorStatus']==1))  
+{
+	echo "Cancel by Patient";
+}
+
+if(($row['userStatus']==1) && ($row['doctorStatus']==0))  
+{
+	echo "Cancel by Doctor";
+}
+
+
+
+												?></td>
+												<td >
+												<div class="visible-md visible-lg hidden-sm hidden-xs">
+							<?php if(($row['userStatus']==1) && ($row['doctorStatus']==1))  
+{ 
+
+													
+echo "No Action yet";
+	 } else {
+
+		echo "Canceled";
+		} ?> </div>
+		
+											
+											<?php 
+$cnt=$cnt+1;
+											 }?>
+											
+											
+										</tbody>
+									</table>
+	<form action="../pdf/index.php" ><button type="submit" name="submit" class="btn btn-o btn-primary">
+															Download PDF
+														</button></form>
+	
+	
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>	
+	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+</body>
+</html>
